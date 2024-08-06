9147cc5bf7d9ffcab4a7fee56243d631a36dcef2
DO YOUNG KIM <dlsrksrlaehdud@naver.com>
2024-08-06 20:54:36 +0900

    엔티티 클래스 생성

---
+++ src/main/java/honeyBee/buzzyBee/Consultant/domain/Consultant.java
@@ -0,0 +1,73 @@
+package honeyBee.buzzyBee.Consultant.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Consultant {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String specialty;
+
+    private String career;
+
+    private String password;
+
+    private String phoneNumber;
+
+    private Double consultationFee; // 요금
+
+    private String hospitalAddress;
+
+    public Consultant() {
+    }
+
+    public Consultant(String name, String specialty, String career, String password, String phoneNumber, Double consultationFee, String hospitalAddress) {
+        this.name = name;
+        this.specialty = specialty;
+        this.career = career;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.consultationFee = consultationFee;
+        this.hospitalAddress = hospitalAddress;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSpecialty() {
+        return specialty;
+    }
+
+    public String getCareer() {
+        return career;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public Double getConsultationFee() {
+        return consultationFee;
+    }
+
+    public String getHospitalAddress() {
+        return hospitalAddress;
+    }
+}
---
+++ src/main/java/honeyBee/buzzyBee/User/domain/User.java
@@ -0,0 +1,59 @@
+package honeyBee.buzzyBee.User.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private String phoneNumber;
+
+    private String ssn;
+
+    public User() {
+    }
+
+    public User(String name, String email, String password, String phoneNumber, String ssn) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+        this.ssn = ssn;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+}
