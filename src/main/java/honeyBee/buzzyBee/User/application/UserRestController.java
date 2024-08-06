6b5f4ca7e8efe86995609fc26900a9bd650ac600
DO YOUNG KIM <dlsrksrlaehdud@naver.com>
2024-08-06 20:55:41 +0900

    RestController 클래스 생성

---
+++ src/main/java/honeyBee/buzzyBee/User/application/UserRestController.java
@@ -0,0 +1,13 @@
+package honeyBee.buzzyBee.User.application;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserRestController {
+
+    private final UserService userService;
+
+    public UserRestController(UserService userService) {
+        this.userService = userService;
+    }
+}
