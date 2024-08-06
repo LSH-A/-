36dbd3a3e67f3f02922a8d0972eebda219464d38
DO YOUNG KIM <dlsrksrlaehdud@naver.com>
2024-08-06 20:55:26 +0900

    리포지토리 클래스 생성

---
+++ src/main/java/honeyBee/buzzyBee/Consultant/domain/ConsultantRepository.java
@@ -0,0 +1,6 @@
+package honeyBee.buzzyBee.Consultant.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConsultantRepository extends JpaRepository<Consultant, Long> {
+}
