9f9e40f22647102279b1afd19a129a1eb59812cb
hee <cyh2616@naver.com>
2024-08-06 21:09:32 +0900

    카테고리/ 상담사카테고리 엔티티,컨트롤러,서비스,리포지토리 생성

---
+++ src/main/java/honeyBee/buzzyBee/Category/CategoryRepository.java
@@ -0,0 +1,6 @@
+package honeyBee.buzzyBee.Category;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
